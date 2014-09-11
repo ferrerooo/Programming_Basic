8fda4a244016b1746ab7904584bc682332077ab0
ferrerooo <ferrerooo@gmail.com>
2014-09-10 23:46:41 -0400

    0910

--- src/main/java/zcy/multithread/MT10_01BlockingQueue.java
+++ src/main/java/zcy/multithread/MT10_01BlockingQueue.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class MT10_01BlockingQueue {
 
 	public static void main(String[] args) throws InterruptedException { 
-		BlockingQueue bqueue = new ArrayBlockingQueue(20); 
+		BlockingQueue<Integer> bqueue = new ArrayBlockingQueue<Integer>(20); 
 		for (int i = 0; i < 30; i++) { 
 			//将指定元素添加到此队列中，如果没有可用空间，将一直等待（如果有必要）。 
 			bqueue.put(i); 
